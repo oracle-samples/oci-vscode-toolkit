2f2d8715875fdf0a678cccbdc6f95fede5dffa17
Varun Mankal <varunmankal@gmail.com>
2025-04-16 10:23:12 -0700

    Revert "release version 1.0.6"
    This reverts commit e3948cb9d496f7f418078e9a407f032c4c6f3d29.

--- src/apm-vscode-plugin/src/common/monitor.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Copyright © 2025, Oracle and/or its affiliates.
- * This software is licensed to you under the Universal Permissive License (UPL) 1.0 as shown at https://oss.oracle.com/licenses/upl.
- */
-import { Service } from "oci-ide-plugin-base/dist/monitoring/service";
-
-export const MONITOR = new Service('apm', '1.0.0', 50);
